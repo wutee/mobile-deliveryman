7ca0b0193b3a3ff79cf73c7088b8c5bf997a7ecd
scresh <screshowski@gmail.com>
2018-12-16 12:42:07 +0100

    Fix order listing

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from './service/order.service';
+import {FoodOrder, FoodOrderResourceService} from '../../client';
+
 
 @Component({
   selector: 'app-orders',
@@ -8,6 +10,10 @@ import { OrderService } from './service/order.service';
 })
 export class OrdersPage {
   orders: any;
+  isList: boolean;
+  clickedOrder = {restaurant: {nameSlug: ''}};
+
+
 
   constructor(public orderService: OrderService) {
     this.getAwaitingOrders();
@@ -15,10 +21,32 @@ export class OrdersPage {
   }
 
   getAwaitingOrders() {
+    this.isList = true;
     this.orderService.getAwaitingOrders()
+      .then((data: FoodOrder[]) => {
+        this.orders = data.filter((order) => (order.status === 2 || order.status === 3) && !!!order.delivery);
+      });
+  }
+
+  assign(orderID) {
+    this.orderService.assignOrder(orderID);
+  }
+
+  search(event) {
+    event.preventDefault();
+    this.orderService.getOrdersWithPhrase(event.target.elements[0].value)
       .then(data => {
         this.orders = data;
       });
+  }
+
+  getDetails(order) {
+    this.isList = false;
+    this.clickedOrder = order;
+  }
+
+  goBack() {
+    this.isList = true;
   }
 
 }
