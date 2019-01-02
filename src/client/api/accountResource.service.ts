ad955cc22cae582d802e483684d52e8717b42799
Dawid Sowa <dawidpawelsowa@gmail.com>
2019-01-02 23:06:28 +0100

    New API adjustments

--- src/client/api/accountResource.service.ts
+++ src/client/api/accountResource.service.ts
@@ -31,7 +31,7 @@ import { Configuration }                                     from '../configurat
 @Injectable()
 export class AccountResourceService {
 
-    protected basePath = 'https://propsy-backend-v0.herokuapp.com';
+    protected basePath = '';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
 
--- src/client/api/deliveryPersonnelResource.service.ts
+++ src/client/api/deliveryPersonnelResource.service.ts
@@ -27,7 +27,7 @@ import { Configuration }                                     from '../configurat
 @Injectable()
 export class DeliveryPersonnelResourceService {
 
-    protected basePath = 'https://propsy-backend-v0.herokuapp.com';
+    protected basePath = '';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
 
